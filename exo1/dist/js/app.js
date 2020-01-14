7d45757b8be12bbcd2348a908c623c8c69d19987
31010-83-06 <31010-83-06@exchange.ad.afpanet>
2020-01-14 11:30:23 +0100

    exo1 finish

--- exo1/dist/js/app.js
+++ exo1/dist/js/app.js
@@ -1,5 +1,29 @@
-window.onload = (function(){
-    //  my code ...
-    console.log(arguments)
-    
-})();
+window.onload = function () {
+    this.clickBtn(this.childBtn())
+};
+function childBtn() {
+    var arrBtn = [];
+    var btn = document.querySelectorAll(".btn");
+    btn.forEach(function (element, i) {
+        arrBtn.push(element)
+    });
+    return arrBtn
+}
+function clickBtn(childBtn) {
+    childBtn.forEach(function (el, i) {
+        var toggle = { isClick: false }
+        if (i === 0) el.onclick = function () {
+            toggle.isClick = !toggle.isClick ? true : false;
+            toggleColor(el.parentNode, toggle.isClick, "red")
+
+        }
+        if (i === 1) el.onclick = function () {
+            toggle.isClick = !toggle.isClick ? true : false;
+            toggleColor(el.parentNode.parentNode, toggle.isClick, "red")
+        }
+    })
+}
+function toggleColor(element, isClick, color) {
+    if (isClick) element.style.backgroundColor = color;
+    else element.style.backgroundColor = "";
+}
